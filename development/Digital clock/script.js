e977a369b48e3113bcb919b04cc710506a348a2f
ANKIT SINGH SHEKHAWAT <ankitnssingh@gmail.com>
2023-10-17 16:52:13 +0530

    digital clock using js

---
+++ development/Digital clock/script.js
@@ -0,0 +1,37 @@
+setInterval(displayTime, 1000);
+
+function displayTime() {
+
+    const timeNow = new Date();
+
+    let hoursOfDay = timeNow.getHours();
+    let minutes = timeNow.getMinutes();
+    let seconds = timeNow.getSeconds();
+    let weekDay = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+    let today = weekDay[timeNow.getDay()];
+    let months = timeNow.toLocaleString("default", {
+        month: "long"
+    });
+    let year = timeNow.getFullYear();
+    let period = "AM";
+
+    if (hoursOfDay > 12) {
+        hoursOfDay-= 12;
+        period = "PM";
+    }
+
+    if (hoursOfDay === 0) {
+        hoursOfDay = 12;
+        period = "AM";
+    }
+
+    hoursOfDay = hoursOfDay < 10 ? "0" + hoursOfDay : hoursOfDay;
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+    let time = hoursOfDay + ":" + minutes + ":" + seconds + period;
+
+    document.getElementById('Clock').innerHTML = time + " " + today + " " + months + " " + year;
+
+}
+displayTime();
